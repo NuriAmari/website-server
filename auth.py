7e149e032f65d65d7458bf789d9aa03a6c8dc75a
NuriAmari <nuri.amari99@gmail.com>
2020-12-27 15:01:28 -0500

    Pull auth credential from env variables

--- auth.py
+++ auth.py
@@ -1,20 +1,20 @@
 import secrets
 import json
+import os
 
 import redis
 from tornado.web import RequestHandler
 
 r = redis.Redis(host="localhost", port=6379, db=0)
 
+username = os.environ.get("WEBSITE_USERNAME")
+password = os.environ.get("WEBSITE_PASSWORD")
+
 
 class AuthHandler(RequestHandler):
-    def get(self):
-        self.write("Hello World")
-
     def post(self):
         data = json.loads(self.request.body)
-        print("aa", data)
-        if data["username"] == "username" and data["password"] == "password":
+        if data["username"] == username and data["password"] == password:
             auth_cookie = secrets.token_urlsafe()
             r.sadd("COOKIES", auth_cookie)
             self.set_secure_cookie("auth", auth_cookie)
