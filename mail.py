a879b6fb8261fd73a4fc4e5a7d05a29bc7126289
NuriAmari <nuri.amari99@gmail.com>
2020-12-27 21:50:28 -0500

    Stop sending email notifications unless in prod

--- mail.py
+++ mail.py
@@ -3,19 +3,20 @@ from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
 def send_notification():
-    message = Mail(
-        from_email=os.environ.get('WEBSITE_EMAIL'),
-        to_emails=os.environ.get('WEBSITE_EMAIL'),
-        subject='Your move pal',
-        html_content="<p>Someone's made a move</p>"
-    )
+    if os.environ.get('MODE') == 'PROD':
+        message = Mail(
+            from_email=os.environ.get('WEBSITE_EMAIL'),
+            to_emails=os.environ.get('WEBSITE_EMAIL'),
+            subject='Your move pal',
+            html_content="<p>Someone's made a move</p>"
+        )
 
-    try:
-        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-        response = sg.send(message)
-        print(response.status_code)
-        print(response.body)
-        print(response.headers)
-    except Exception as e:
-        print(e.message)
+        try:
+            sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+            response = sg.send(message)
+            print(response.status_code)
+            print(response.body)
+            print(response.headers)
+        except Exception as e:
+            print(e.message)
 
